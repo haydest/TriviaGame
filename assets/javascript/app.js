2acf14fbf9b1421f5a8b1434da3da755c59948e9
Hayden <haydenestes@marys-mbp-2.attlocal.net>
2018-09-19 18:55:05 -0400

    wip 1.1

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+
+
+var time = document.getElementById("time-remaining");
+var questions = document.getElementById("questions");
+
+$("#time-remaining").append("<button>");
+$("<button>").text("click here to start the game!");
+
+
+
+
